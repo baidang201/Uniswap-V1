485022f47eb72e58e46e0e2ace9ee443d22e06df
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-13 12:56:03 +0100

    LP reserve function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,5 +4,16 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Exchange is ERC20 {
-    
+    address public tokenAddress;
+    // Exchange is inheriting ERC20, because our exchange itself is an ERC-20 contract
+    // as it is responsible for minting and issuing LP Tokens
+    constructor(address token) ERC20("ETH TOKEN LP Token", "lpETHTOKEN") {
+        require(token != address(0), "Token address passed is a null address");
+        tokenAddress = token;
+    }
+
+    // getReserve returns the balance of `token` held by `this` contract
+    function getReserve() public view returns (uint256) {
+        return ERC20(tokenAddress).balanceOf(address(this));
+    }
 }
