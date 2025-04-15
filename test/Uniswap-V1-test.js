7cedabe30b342379a103b899596a14a1bca5ae0a
Li Smith <jinrishouji@sina.com>
2025-04-15 23:57:38 +0800

    fix test

--- test/Uniswap-V1-test.js
+++ test/Uniswap-V1-test.js
@@ -12,35 +12,41 @@ describe("Exchange", function (){
         [owner,user1,user2] = await ethers.getSigners();
         
         const Token = await ethers.getContractFactory("Token");
-        token = await Token.deploy("Token", "TKN");
-        await token.deploy();
-
+        token = await Token.deploy();
+        await token.deploymentTransaction().wait();
+        
         const Exchange = await ethers.getContractFactory("Exchange");
-        exchange = await Exchange.deploy(token.address);
-        await exchange.deploy();
+        exchange = await Exchange.deploy(token.target);
+        await exchange.deploymentTransaction().wait();
+        
+        // 从owner向user1转移200个代币用于测试
+        await token.connect(owner).transfer(user1.address, ethers.parseEther("200"));
+        
+        // 如果后续测试中user2也需要代币，也要转移给user2
+        await token.connect(owner).transfer(user2.address, ethers.parseEther("200"));
     });
 
     it("should deploy with correct initializaton state", async function(){
-        expect(await exchange.tokenAddress()).to.equal(token.address);
-        expect(await exchange.name()).to.equal("Token");
-        expect(await exchange.symbol()).to.equal("TKN");
+        expect(await exchange.tokenAddress()).to.equal(token.target);
+        expect(await exchange.name()).to.equal("ETH TOKEN LP Token");
+        expect(await exchange.symbol()).to.equal("lpETHTOKEN");
     });
 
     it("should allow user to add liquidity", async function(){
-        const amountOfToken = ethers.utils.parseEther("100");
+        const amountOfToken = ethers.parseEther("100");
 
-        await token.connect(user1).approve(exchange.address, amountOfToken);
-        await exchange.connect(user1).addLiquidity(amountOfToken, { value: ethers.utils.parseEther("1") });
+        await token.connect(user1).approve(exchange.target, amountOfToken);
+        await exchange.connect(user1).addLiquidity(amountOfToken, { value: ethers.parseEther("1") });
 
         const userLPTokenBalance = await exchange.balanceOf(user1.address);
         expect(userLPTokenBalance).to.not.equal(0);
     });
 
     it("should allow users to remove liquidity", async function () {
-        const amountOfToken = ethers.utils.parseEther("100");
-        const ethValue = ethers.utils.parseEther("1");
+        const amountOfToken = ethers.parseEther("100");
+        const ethValue = ethers.parseEther("1");
     
-        await token.connect(user1).approve(exchange.address, amountOfToken);
+        await token.connect(user1).approve(exchange.target, amountOfToken);
         await exchange.connect(user1).addLiquidity(amountOfToken, { value: ethValue });
     
         const userLPTokenBalanceBefore = await exchange.balanceOf(user1.address);
@@ -52,21 +58,24 @@ describe("Exchange", function (){
     });
 
     it("should allow users to swap tokens for ETH", async function () {
-        const amountOfToken = ethers.utils.parseEther("100");
+        const amountOfToken = ethers.parseEther("100");
     
-        await token.connect(user1).approve(exchange.address, amountOfToken);
-        await exchange.connect(user1).addLiquidity(amountOfToken, { value: ethers.utils.parseEther("1") });
+        await token.connect(user1).approve(exchange.target, amountOfToken);
+        await exchange.connect(user1).addLiquidity(amountOfToken, { value: ethers.parseEther("1") });
     
         const tokenReserveBefore = await exchange.getReserve();
-        const userEthBalanceBefore = await user2.getBalance();
-    
+        
+        const userEthBalanceBefore = await ethers.provider.getBalance(user2.address);
+        
+        await token.connect(user2).approve(exchange.target, amountOfToken);
         await exchange.connect(user2).tokenToEthSwap(amountOfToken, 0);
     
         const tokenReserveAfter = await exchange.getReserve();
-        const userEthBalanceAfter = await user2.getBalance();
+        
+        const userEthBalanceAfter = await ethers.provider.getBalance(user2.address);
     
         expect(userEthBalanceAfter).to.be.above(userEthBalanceBefore);
-        expect(tokenReserveAfter).to.be.below(tokenReserveBefore);
+        expect(tokenReserveAfter).to.be.above(tokenReserveBefore);
     });
 });
      
